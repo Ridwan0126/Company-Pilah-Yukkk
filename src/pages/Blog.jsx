0db5232ef0ea85eb2ee3217024722c5bea3a351b
Ridwan0126 <muhamadridwan6792@gmail.com>
2024-11-15 22:45:11 +0700

    cmtt

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,22 @@
+import BannerSampah from "../assets/BannerBlog.png"; 
+import BlogImage1 from "../assets/blog1.png"; 
+import BlogImage2 from "../assets/blog2.png";
+import BlogImage3 from "../assets/blog3.png";
+import Blogs from "../components/Blogs"; 
+
+export default function Blog() {
+
+  const images = [BlogImage1, BlogImage2, BlogImage3];
+
+  const blogs = Array.from({ length: 12 }).map((_, idx) => ({
+    id: idx + 1,
+    title: `Kuy Point: Cara Kumpul & Tukar Hadiah`,
+    description: `Daur ulang dan dapatkan hadiah! Simak cara mudah mengumpulkan Kuy Point dan menukarkannya dengan reward menarik.`,
+    image: images[idx % 3], 
+    link: "#", 
+  }));
+
+  return (
+    <Blogs blogs={blogs} banner={BannerSampah} />
+  );
+}
